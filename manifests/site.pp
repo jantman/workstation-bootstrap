3f4e6ff7dea7f0728eb8da3019e38237fce91ddd
Jason Antman <Jason.Antman@coxinc.com>
2014-03-17 20:05:48 -0400

    pull in archlinux_workstation

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,16 @@
 # https://github.com/jantman/workstation-bootstrap/blob/production/manifests/site.pp
 
-###########################################################
-# global config - stuff that should be useful to everyone #
-###########################################################
+#################
+# configuration #
+#################
+
+###########################################
+# stuff that should be useful to everyone #
+###########################################
 if $::osfamily == 'Archlinux' {
   # Arch-specific stuff
+  class {'archlinux_workstation':
+  }
 
   if $::type == 'Notebook' or $::type == 'Portable' or $::type == 'LapTop' or $::type == 'Sub Notebook' {
     # Arch laptop specific
