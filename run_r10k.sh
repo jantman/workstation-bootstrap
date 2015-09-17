0ab7698fa429bf4fbade1fc98835c89540bfc613
Jason Antman <jason@jasonantman.com>
2015-09-17 16:06:32 -0400

    fix r10k argument passing

--- run_r10k.sh
+++ run_r10k.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-PUPPETFILE=/etc/puppetlabs/code/workstation-bootstrap/puppet/Puppetfile PUPPETFILE_DIR=/etc/puppetlabs/code/workstation-bootstrap/puppet/modules /usr/bin/r10k $@ puppetfile install
+PUPPETFILE=/etc/puppetlabs/code/workstation-bootstrap/puppet/Puppetfile PUPPETFILE_DIR=/etc/puppetlabs/code/workstation-bootstrap/puppet/modules /usr/bin/r10k puppetfile install $@
