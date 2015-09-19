4e5d66c328c3ad6766b9bff2299cf615181f2728
Jason Antman <jason@jasonantman.com>
2015-09-19 11:44:02 -0400

    symlink in the manifest to spec fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+spec_path = File.join(proj_root, 'spec', 'fixtures', 'manifests', 'site.pp')
+sitepp_path = File.join(proj_root, 'puppet', 'manifests', 'site.pp')
+unless File.exists?(spec_path)
+  FileUtils.ln_s(sitepp_path, spec_path)
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
