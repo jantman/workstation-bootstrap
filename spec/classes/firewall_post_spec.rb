c7f167d87b592bbb7af43847c1d8be03fb76fddc
Jason Antman <jason@jasonantman.com>
2015-09-20 10:42:09 -0400

    add my privatepuppet module

--- spec/classes/firewall_post_spec.rb
+++ spec/classes/firewall_post_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe 'workstation_bootstrap::firewall_post' do
-  let(:pre_condition) { 'class archlinux_workstation ($username, ) {}' }
-  let(:pre_condition) { 'class privatepuppet {}' }
-
   let(:facts) {{
     :osfamily => 'RedHat',
     :kernel   => 'Linux',
