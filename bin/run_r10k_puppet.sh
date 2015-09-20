19c000357d3778d89106eae3585e913db4091717
Jason Antman <jason@jasonantman.com>
2015-09-19 23:14:08 -0400

    fix typo in paths

--- bin/run_r10k_puppet.sh
+++ bin/run_r10k_puppet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -x
-/etc/puppetlabs/code/workstation_bootstrap/bin/run_r10k.sh && /etc/puppetlabs/code/workstation_bootstrap/bin/run_puppet.sh $@
+/etc/puppetlabs/code/workstation-bootstrap/bin/run_r10k.sh && /etc/puppetlabs/code/workstation-bootstrap/bin/run_puppet.sh $@
