991d4ec67fdb5743aed5a745b5b3c35a40786d68
Jason Antman <jason@jasonantman.com>
2016-08-03 07:58:32 -0400

    git fetch and pull before running r10k

--- bin/run_r10k.sh
+++ bin/run_r10k.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -x
-
+pushd /etc/puppetlabs/code/workstation-bootstrap/puppet/
+git fetch && git pull
+popd
 PUPPETFILE=/etc/puppetlabs/code/workstation-bootstrap/puppet/Puppetfile PUPPETFILE_DIR=/etc/puppetlabs/code/workstation-bootstrap/puppet/modules /usr/bin/r10k puppetfile install $@
 ln -s /etc/puppetlabs/code/workstation-bootstrap/modules/workstation_bootstrap /etc/puppetlabs/code/workstation-bootstrap/puppet/modules/workstation_bootstrap
