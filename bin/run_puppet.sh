3a384eb97c868e925b52c558071240795cee2ec6
Jason Antman <jason@jasonantman.com>
2020-04-10 16:46:17 -0400

    run puppet with RUBYOPT="-W0" to suppress warnings on arch linux

--- bin/run_puppet.sh
+++ bin/run_puppet.sh
@@ -6,7 +6,14 @@ cd $CLONEDIR
 
 sed "s|%%CLONEDIR%%|${CLONEDIR}|g" hiera.yaml.template > hiera.yaml
 
-puppet apply --verbose $@ \
-  --hiera_config=${CLONEDIR}/hiera.yaml \
-  --modulepath="${CLONEDIR}/modules/local:${CLONEDIR}/modules/r10k" \
-  ${CLONEDIR}/manifests/site.pp
+if [[ -e /etc/arch-release ]]; then
+  RUBYOPT="-W0" puppet apply --verbose $@ \
+    --hiera_config=${CLONEDIR}/hiera.yaml \
+    --modulepath="${CLONEDIR}/modules/local:${CLONEDIR}/modules/r10k" \
+    ${CLONEDIR}/manifests/site.pp
+else
+  puppet apply --verbose $@ \
+    --hiera_config=${CLONEDIR}/hiera.yaml \
+    --modulepath="${CLONEDIR}/modules/local:${CLONEDIR}/modules/r10k" \
+    ${CLONEDIR}/manifests/site.pp
+fi
