f365d4b140b888a0242d3d393f533b74bee8cdfd
Jason Antman <jason@jasonantman.com>
2023-01-07 18:19:21 -0500

    log dropped packets from iptables

--- modules/local/workstation_bootstrap/manifests/firewall_post.pp
+++ modules/local/workstation_bootstrap/manifests/firewall_post.pp
@@ -1,8 +1,28 @@
 # set default puppetlabs-firewall drop rule
 class workstation_bootstrap::firewall_post {
-  firewall { '999 drop all':
+  # create a new chain to log and then drop
+  firewallchain {'LOGDROP:filter:IPv4':
+    ensure => present,
+  }
+  -> firewall {'1 LOGDROP-log':
+    chain      => 'LOGDROP',
+    limit      => '2/min',
+    jump       => 'LOG',
+    log_level  => '4',
+    log_uid    => true,
+    log_prefix => 'IPTables-LOGDROP: ',
+    before => undef,
+  }
+  -> firewall {'2 LOGDROP-drop':
+    chain  => 'LOGDROP',
     proto  => 'all',
     action => 'drop',
+    before => undef,
+  }
+
+  # now on the default chain, as the final rule, jump to LOGDROP
+  firewall { '999 drop all and log':
+    jump => 'LOGDROP',
     before => undef,
   }
 }
